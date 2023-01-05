4ff175ea5db6ae34e7a4ac8a1900a959d939a62a
Rahul-Naikwade <rahulnaikwade94@gmail.com>
2023-01-05 13:44:33 +0530

    i have added code in main method ...start and end

--- src/main/java/com/demo/_11_Spring_BootExample/Application.java
+++ src/main/java/com/demo/_11_Spring_BootExample/Application.java
@@ -8,9 +8,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Main ...start....!!");
+System.out.println("Main ...start....!!");
 		SpringApplication.run(Application.class, args); //
 
 		System.out.println("Main ...end....!!");
-	}
+		System.out.println("Main ...end....!!");
+	
+}
 
 }
